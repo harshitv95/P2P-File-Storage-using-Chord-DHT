1e9538fb71902e0f5cb6b03d0a47c13675a8dd7c
Harshit Vadodaria <harshitv95@gmail.com>
2020-10-28 17:16:24 -0400

    +minor debug logging

--- ChordDHT/src/main/java/com/hvadoda1/dht/chord/rpc/thrift/connect/ThriftConfig.java
+++ ChordDHT/src/main/java/com/hvadoda1/dht/chord/rpc/thrift/connect/ThriftConfig.java
@@ -3,6 +3,7 @@ package com.hvadoda1.dht.chord.rpc.thrift.connect;
 import java.util.Map;
 
 import com.hvadoda1.dht.chord.Config;
+import com.hvadoda1.dht.chord.util.Logger;
 
 public class ThriftConfig extends Config {
 
@@ -11,38 +12,50 @@ public class ThriftConfig extends Config {
 	}
 
 	public static String getKeyStorePath() {
-		return getProperty("KEYSTORE_PATH");
+		String s = getProperty("KEYSTORE_PATH");
+		Logger.debugLow("Using KeyStore [" + s + "]");
+		return s;
 	}
 
 	public static String getKeyStorePassword() {
-		return getProperty("KEYSTORE_PASSWORD");
+		String s = getProperty("KEYSTORE_PASSWORD");
+		Logger.debugLow("Using KeyStore Password [" + s + "]");
+		return s;
 	}
 
 	public static String getKeyStoreManager() {
 		String s = getProperty("KEYSTORE_MANAGER");
+		Logger.debugLow("Using KeyStore Manager [" + s + "]");
 		return s == null || s.trim().isEmpty() ? null : s;
 	}
 
 	public static String getKeyStoreType() {
 		String s = getProperty("KEYSTORE_TYPE");
+		Logger.debugLow("Using KeyStore Type [" + s + "]");
 		return s == null || s.trim().isEmpty() ? null : s;
 	}
 
 	public static String getTrustStorePath() {
-		return getProperty("TRUSTSTORE_PATH");
+		String s = getProperty("TRUSTSTORE_PATH");
+		Logger.debugLow("Using TrustStore [" + s + "]");
+		return s;
 	}
 
 	public static String getTrustStorePassword() {
-		return getProperty("TRUSTSTORE_PASSWORD");
+		String s = getProperty("TRUSTSTORE_PASSWORD");
+		Logger.debugLow("Using TrustStore Type [" + s + "]");
+		return s;
 	}
 
 	public static String getTrustStoreManager() {
 		String s = getProperty("TRUSTSTORE_MANAGER");
+		Logger.debugLow("Using TrustStore Manager [" + s + "]");
 		return s == null || s.trim().isEmpty() ? null : s;
 	}
 
 	public static String getTrustStoreType() {
 		String s = getProperty("TRUSTSTORE_TYPE");
+		Logger.debugLow("Using TrustStore Type [" + s + "]");
 		return s == null || s.trim().isEmpty() ? null : s;
 	}
 
